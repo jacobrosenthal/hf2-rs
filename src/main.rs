1c2537a87c77aacc285b4146763a74a74a51d313
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-10-11 16:49:10 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use core::convert::TryFrom;
 use hidapi::HidApi;
 use scroll::{ctx, Pread, Pwrite, LE};
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum CommandId {
     // This command states the current mode of the device:
     BinInfo = 0x0001,
@@ -15,13 +15,13 @@ enum CommandId {
     ResetIntoBootloader = 0x0004,
     // When issued in bootloader mode, it has no effect. In user-space mode it causes handover to bootloader. A BININFO command can be issued to verify that.
     StartFlash = 0x0005,
-    //Write a single page of flash memory.
+    //Write a single page of flash memory. No Result.
     WriteFlashPage = 0x0006,
     //Compute checksum of a number of pages. Maximum value for num_pages is max_message_size / 2 - 2. The checksum algorithm used is CRC-16-CCITT.
     Checksum = 0x0007,
     //Read a number of words from memory. Memory is read word by word (and not byte by byte), and target_addr must be suitably aligned. This is to support reading of special IO regions.
     ReadWords = 0x0008,
-    //Dual of READ WORDS, with the same constraints.
+    //Dual of READ WORDS, with the same constraints. No Result.
     WriteWords = 0x0009,
     //Return internal log buffer if any. The result is a character array.
     Dmesg = 0x0010,
@@ -47,25 +47,16 @@ impl TryFrom<u32> for BinInfoMode {
     }
 }
 
-trait CommanderResult {}
-
-trait Commander<RES: CommanderResult> {
-    const ID: CommandId;
-
-    fn transfer(&self, d: &hidapi::HidDevice) -> Result<RES, Error>;
-}
-
+// This command states the current mode of the device:
 struct BinInfo {}
 
 impl Commander<BinInfoResult> for BinInfo {
-    const ID: CommandId = CommandId::BinInfo;
+    fn send(&self, d: &hidapi::HidDevice) -> Result<BinInfoResult, Error> {
+        let bitsnbytes = transfer(CommandId::BinInfo, d, &[0])?;
 
-    fn transfer(&self, d: &hidapi::HidDevice) -> Result<BinInfoResult, Error> {
-        let bitsnbytes = something(Self::ID, d)?;
+        let res: BinInfoResult = (bitsnbytes.as_slice()).pread_with::<BinInfoResult>(0, LE)?;
 
-        let info: BinInfoResult = (bitsnbytes.as_slice()).pread_with::<BinInfoResult>(0, LE)?;
-
-        Ok(info)
+        Ok(res)
     }
 }
 
@@ -77,21 +68,20 @@ struct BinInfoResult {
     max_message_size: u32,
     family_id: Option<u32>, // optional
 }
-
 impl CommanderResult for BinInfoResult {}
 
 impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for BinInfoResult {
-    type Error = scroll::Error;
+    type Error = Error;
     fn try_from_ctx(this: &'a [u8], le: scroll::Endian) -> Result<(Self, usize), Self::Error> {
         if this.len() < 16 {
-            return Err((scroll::Error::Custom("whatever".to_string())).into());
+            return Err(Error::Parse);
         }
 
         //does it give me offset somehow??? or just slice appropriately for me?s
         let mut offset = 0;
         let mode: u32 = this.gread_with::<u32>(&mut offset, le)?;
         let mode: Result<BinInfoMode, Error> = BinInfoMode::try_from(mode);
-        let mode: BinInfoMode = mode.unwrap();
+        let mode: BinInfoMode = mode?;
         let flash_page_size = this.gread_with::<u32>(&mut offset, le)?;
         let flash_num_pages = this.gread_with::<u32>(&mut offset, le)?;
         let max_message_size = this.gread_with::<u32>(&mut offset, le)?;
@@ -116,16 +106,15 @@ impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for BinInfoResult {
     }
 }
 
+// Various device information. The result is a character array. See INFO_UF2.TXT in UF2 format for details.
 struct Info {}
 impl Commander<InfoResult> for Info {
-    const ID: CommandId = CommandId::Info;
+    fn send(&self, d: &hidapi::HidDevice) -> Result<InfoResult, Error> {
+        let bitsnbytes = transfer(CommandId::Info, d, &[0])?;
 
-    fn transfer(&self, d: &hidapi::HidDevice) -> Result<InfoResult, Error> {
-        let bitsnbytes = something(Self::ID, d)?;
+        let res: InfoResult = (bitsnbytes.as_slice()).pread_with::<InfoResult>(0, LE)?;
 
-        let info: InfoResult = (bitsnbytes.as_slice()).pread_with::<InfoResult>(0, LE)?;
-
-        Ok(info)
+        Ok(res)
     }
 }
 
@@ -136,27 +125,58 @@ struct InfoResult {
 impl CommanderResult for InfoResult {}
 
 impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for InfoResult {
-    type Error = scroll::Error;
+    type Error = Error;
     fn try_from_ctx(this: &'a [u8], le: scroll::Endian) -> Result<(Self, usize), Self::Error> {
         let mut bytes = vec![0; this.len()];
 
         let mut offset = 0;
-        this.gread_inout_with(&mut offset, &mut bytes, LE)?;
+        this.gread_inout_with(&mut offset, &mut bytes, le)?;
 
-        let info = std::str::from_utf8(&bytes).unwrap();
+        let info = std::str::from_utf8(&bytes)?;
 
         Ok((InfoResult { info: info.into() }, offset))
     }
 }
 
-struct WriteFlashPageCommand {
+//Write a single page of flash memory. No Result.
+struct WriteFlashPage {
     target_addr: u32,
     data: Vec<u8>,
 }
 
-struct ChksumPagesCommand {
+impl Commander<NoResult> for WriteFlashPage {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<NoResult, Error> {
+        let data = &mut [0_u8; 64];
+
+        let mut offset = 0;
+
+        data.gwrite_with(self.target_addr, &mut offset, LE)?;
+
+        for i in &self.data {
+            data.gwrite_with(i, &mut offset, LE)?;
+        }
+
+        let _ = transfer(CommandId::WriteFlashPage, d, &data[..offset])?;
+
+        Ok(NoResult {})
+    }
+}
+
+//Compute checksum of a number of pages. Maximum value for num_pages is max_message_size / 2 - 2. The checksum algorithm used is CRC-16-CCITT.
+struct ChksumPages {
     target_addr: u32,
     num_pages: u32,
+}
+
+impl Commander<ChksumPagesResult> for ChksumPages {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<ChksumPagesResult, Error> {
+        let bitsnbytes = transfer(CommandId::Checksum, d, &[0])?;
+
+        let res: ChksumPagesResult =
+            (bitsnbytes.as_slice()).pread_with::<ChksumPagesResult>(0, LE)?;
+
+        Ok(res)
+    }
 }
 
 //Maximum value for num_pages is max_message_size / 2 - 2. The checksum algorithm used is CRC-16-CCITT.
@@ -164,38 +184,158 @@ struct ChksumPagesCommand {
 struct ChksumPagesResult {
     chksums: Vec<u16>,
 }
+impl CommanderResult for ChksumPagesResult {}
 
-struct ReadWordsCommand {
+impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for ChksumPagesResult {
+    type Error = Error;
+    fn try_from_ctx(this: &'a [u8], le: scroll::Endian) -> Result<(Self, usize), Self::Error> {
+        let mut chksums: Vec<u16> = vec![0; this.len() / 2];
+
+        let mut offset = 0;
+        this.gread_inout_with(&mut offset, &mut chksums, le)?;
+
+        Ok((ChksumPagesResult { chksums }, offset))
+    }
+}
+
+//Reset the device into user-space app. Usually, no response at all will arrive for this command.
+struct ResetIntoApp {}
+impl Commander<NoResult> for ResetIntoApp {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<NoResult, Error> {
+        let _ = transfer(CommandId::ResetIntoApp, d, &[0])?;
+
+        Ok(NoResult {})
+    }
+}
+
+//Reset the device into bootloader, usually for flashing. Usually, no response at all will arrive for this command.
+struct ResetIntoBootloader {}
+impl Commander<NoResult> for ResetIntoBootloader {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<NoResult, Error> {
+        let _ = transfer(CommandId::ResetIntoBootloader, d, &[0])?;
+
+        Ok(NoResult {})
+    }
+}
+
+// When issued in bootloader mode, it has no effect. In user-space mode it causes handover to bootloader. A BININFO command can be issued to verify that.
+struct StartFlash {}
+impl Commander<NoResult> for StartFlash {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<NoResult, Error> {
+        let _ = transfer(CommandId::StartFlash, d, &[0])?;
+
+        Ok(NoResult {})
+    }
+}
+
+//Read a number of words from memory. Memory is read word by word (and not byte by byte), and target_addr must be suitably aligned. This is to support reading of special IO regions.
+struct ReadWords {
     target_addr: u32,
     num_words: u32,
+}
+
+impl Commander<ReadWordsResult> for ReadWords {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<ReadWordsResult, Error> {
+        let data = &mut [0_u8; 8];
+
+        let mut offset = 0;
+
+        data.gwrite_with(self.target_addr, &mut offset, LE)?;
+        data.gwrite_with(self.num_words, &mut offset, LE)?;
+
+        let bitsnbytes = transfer(CommandId::ReadWords, d, &data[..])?;
+
+        let res: ReadWordsResult = (bitsnbytes.as_slice()).pread_with::<ReadWordsResult>(0, LE)?;
+
+        Ok(res)
+    }
 }
 
 #[derive(Debug)]
 struct ReadWordsResult {
     words: Vec<u32>,
 }
+impl CommanderResult for ReadWordsResult {}
 
-struct WriteWordsCommand {
+impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for ReadWordsResult {
+    type Error = Error;
+    fn try_from_ctx(this: &'a [u8], le: scroll::Endian) -> Result<(Self, usize), Self::Error> {
+        let mut words: Vec<u32> = vec![0; this.len() / 4];
+
+        let mut offset = 0;
+        this.gread_inout_with(&mut offset, &mut words, le)?;
+
+        Ok((ReadWordsResult { words }, offset))
+    }
+}
+
+//Dual of READ WORDS, with the same constraints. No Result.
+struct WriteWords {
     target_addr: u32,
     num_words: u32,
     words: Vec<u32>,
 }
 
-// no arguments
+impl Commander<NoResult> for WriteWords {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<NoResult, Error> {
+        let data = &mut [0_u8; 64];
+
+        let mut offset = 0;
+
+        data.gwrite_with(self.target_addr, &mut offset, LE)?;
+        data.gwrite_with(self.num_words, &mut offset, LE)?;
+
+        for i in &self.words {
+            data.gwrite_with(i, &mut offset, LE)?;
+        }
+
+        let _ = transfer(CommandId::WriteWords, d, &data[..offset])?;
+
+        Ok(NoResult {})
+    }
+}
+
+//Return internal log buffer if any. The result is a character array.
+struct Dmesg {}
+
+impl Commander<DmesgResult> for Dmesg {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<DmesgResult, Error> {
+        let bitsnbytes = transfer(CommandId::Dmesg, d, &[0])?;
+
+        let res: DmesgResult = (bitsnbytes.as_slice()).pread_with::<DmesgResult>(0, LE)?;
+
+        Ok(res)
+    }
+}
+
 #[derive(Debug)]
 struct DmesgResult {
     logs: String,
 }
+impl CommanderResult for DmesgResult {}
 
-struct Command {
-    command_id: CommandId, //    uint32_t command_id;
-    //arbitrary number set by the host, for example as sequence number. The response should repeat the tag.
-    tag: u16,
-    //The two reserved bytes in the command should be sent as zero and ignored by the device
-    _reserved0: u8,
-    _reserved1: u8,
-    // data: Vec<u8>,
+impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for DmesgResult {
+    type Error = Error;
+    fn try_from_ctx(this: &'a [u8], le: scroll::Endian) -> Result<(Self, usize), Self::Error> {
+        let mut bytes = vec![0; this.len()];
+
+        let mut offset = 0;
+        this.gread_inout_with(&mut offset, &mut bytes, le)?;
+
+        let logs = std::str::from_utf8(&bytes)?;
+
+        Ok((DmesgResult { logs: logs.into() }, offset))
+    }
 }
+
+trait CommanderResult {}
+
+trait Commander<RES: CommanderResult> {
+    fn send(&self, d: &hidapi::HidDevice) -> Result<RES, Error>;
+}
+
+struct NoResult {}
+impl CommanderResult for NoResult {}
 
 #[derive(Debug)]
 struct CommandResponse {
@@ -204,30 +344,8 @@ struct CommandResponse {
     status: CommandResponseStatus, //    uint8_t status;
 
     //additional information In case of non-zero status
-    status_info: u8,
-    // data: Vec<u8>,
-}
-
-impl<'a> ::scroll::ctx::TryIntoCtx<::scroll::Endian> for &'a Command {
-    type Error = ::scroll::Error;
-
-    fn try_into_ctx(
-        self,
-        dst: &mut [u8],
-        ctx: ::scroll::Endian,
-    ) -> ::scroll::export::result::Result<usize, Self::Error> {
-        let mut offset = 0;
-        dst.gwrite_with(self.command_id as u32, &mut offset, ctx)?;
-        dst.gwrite_with(&self.tag, &mut offset, ctx)?;
-        dst.gwrite_with(&self._reserved0, &mut offset, ctx)?;
-        dst.gwrite_with(&self._reserved1, &mut offset, ctx)?;
-
-        // for item in &self.data {
-        //     dst.gwrite_with(item, &mut offset, ctx)?;
-        // }
-
-        Ok(offset)
-    }
+    status_info: u8, // optional?
+                     // data: Vec<u8>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -279,61 +397,61 @@ impl TryFrom<u8> for PacketType {
     }
 }
 
-// doesnt know what the dta is supposed to be decoded as
-// thats linked via the seq number outside,  so we cant decode here
+// doesnt know what the data is supposed to be decoded as
+// thats linked via the seq number outside, so we cant decode here
 impl<'a> ctx::TryFromCtx<'a, scroll::Endian> for CommandResponse {
-    type Error = scroll::Error;
+    type Error = Error;
     fn try_from_ctx(this: &'a [u8], le: scroll::Endian) -> Result<(Self, usize), Self::Error> {
-        println!("len{:?}", this.len());
-
-        if this.len() < 8 {
-            return Err((scroll::Error::Custom("whatever".to_string())).into());
+        if this.len() < 4 {
+            return Err(Error::Parse);
         }
 
         let mut offset = 0;
         let tag = this.gread_with::<u16>(&mut offset, le)?;
         let status: u8 = this.gread_with::<u8>(&mut offset, le)?;
-        let status = CommandResponseStatus::try_from(status).unwrap();
+        let status = CommandResponseStatus::try_from(status)?;
         let status_info = this.gread_with::<u8>(&mut offset, le)?;
-        // let mut data: Vec<u8> = vec![];
-        // this.gread_inout_with(&mut offset, &mut data, le)?;
-
-        // for item in &self.data {
-        //     dst.gwrite_with(item, &mut offset, ctx)?;
-        // }
 
         Ok((
             CommandResponse {
                 tag,
                 status,
                 status_info,
-                // data,
             },
             offset,
         ))
     }
 }
 
-fn something(id: CommandId, d: &hidapi::HidDevice) -> Result<Vec<u8>, Error> {
+fn transfer(id: CommandId, d: &hidapi::HidDevice, data: &[u8]) -> Result<Vec<u8>, Error> {
     let mut seq: u16 = 1;
 
-    let command = Command {
-        command_id: id,
-        //arbitrary number set by the host, for example as sequence number. The response should repeat the tag.
-        tag: seq,
-        //The two reserved bytes in the command should be sent as zero and ignored by the device
-        _reserved0: 0,
-        _reserved1: 0,
-        // data: vec![],
-    };
-    let buffer = &mut [0; 64];
+    let buffer = &mut [0; 68];
 
-    let bytes = buffer.pwrite_with(&command, 1, LE)?;
-    buffer[0] = (PacketType::Final as u8) << 6 | bytes as u8;
+    let mut offset = 1;
 
-    d.write(buffer)?;
+    buffer.gwrite_with(id as u32, &mut offset, LE)?;
+    buffer.gwrite_with(seq, &mut offset, LE)?;
+    buffer.gwrite_with(0, &mut offset, LE)?;
+    buffer.gwrite_with(0, &mut offset, LE)?;
+
+    buffer[0] = (PacketType::Final as u8) << 6 | offset as u8;
+
+    //todo, dont
+    let first_and_last = [&buffer[..offset], &data[..]].concat();
+
+    d.write(first_and_last.as_slice())?;
 
     let mut bitsnbytes: Vec<u8> = vec![];
+
+    //exit early for some commands
+    if id == CommandId::ResetIntoApp
+        || id == CommandId::ResetIntoBootloader
+        || id == CommandId::WriteFlashPage
+        || id == CommandId::WriteWords
+    {
+        return Ok(vec![]);
+    }
 
     //if inner, need to buffer more packets
     let mut ptype = PacketType::Inner;
@@ -341,7 +459,7 @@ fn something(id: CommandId, d: &hidapi::HidDevice) -> Result<Vec<u8>, Error> {
         d.read(buffer)?;
         println!("Receive response: {:02X?}", &buffer[..]);
 
-        ptype = PacketType::try_from(buffer[0] >> 6).unwrap();
+        ptype = PacketType::try_from(buffer[0] >> 6)?;
         let len: usize = (buffer[0] & 0x3F) as usize;
 
         //skip the header byte
@@ -386,24 +504,63 @@ impl From<scroll::Error> for Error {
     }
 }
 
-// enum UF2Result {
-//     BinInfo(BinInfoResult),
-//     Info(InfoResult),
-//     ChksumPages(ChksumPagesResult),
-//     ReadWords(ReadWordsResult),
-//     Dmesg(DmesgResult),
-// }
+impl From<std::str::Utf8Error> for Error {
+    fn from(_err: std::str::Utf8Error) -> Self {
+        Error::Parse
+    }
+}
 
 fn main() -> Result<(), Error> {
     let api = HidApi::new().expect("Couldn't find system usb");
 
     let d = api.open(0x239A, 0x003D).expect("Couldn't find usb device");
 
-    let bininfo: BinInfoResult = BinInfo {}.transfer(&d)?;
+    let bininfo: BinInfoResult = BinInfo {}.send(&d)?;
     println!("{:?}", bininfo);
+    //then total kb is flash_num_pages * flash_page_size / 1024
+    println!(
+        "{:?}kb",
+        bininfo.flash_num_pages * bininfo.flash_page_size / 1024
+    );
 
-    let info: InfoResult = Info {}.transfer(&d)?;
+    let info: InfoResult = Info {}.send(&d)?;
     println!("{:?}", info);
+
+    // not supported on my board
+    // let dmesg: DmesgResult = Dmesg {}.send(&d)?;
+    // println!("{:?}", dmesg);
+
+    // let _ = ResetIntoApp {}.send(&d)?;
+    // let _ = ResetIntoBootloader {}.send(&d)?;
+    //no idea what this does
+    // let _ = StartFlash {}.send(&d)?;
+    // let _ = WriteFlashPage {
+    //     target_addr: 0,
+    //     data: vec![],
+    // }
+    // .send(&d)?;
+
+    // let chk: ChksumPagesResult = ChksumPages {
+    //     target_addr: 0,
+    //     num_pages: 1,
+    // }
+    // .send(&d)?;
+    // println!("{:?}", chk);
+
+    // //no worky?
+    // let words: ReadWordsResult = ReadWords {
+    //     target_addr: 0x4000,
+    //     num_words: 12,
+    // }
+    // .send(&d)?;
+    // println!("{:?}", words);
+
+    // let _ = WriteWords {
+    //     target_addr: 0,
+    //     num_words: 1,
+    //     words: vec![],
+    // }
+    // .send(&d)?;
 
     Ok(())
 }
